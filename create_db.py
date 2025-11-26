20ddfadbaa02cc32233797d3387ae3bea693a91f
jacob sun <jacobsun211@gmail.com>
2025-11-26 12:44:02 +0200

    save

--- create_db.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, Field, create_engine, Session, select
-
-
-
-engine = create_engine("sqlite:///database.db")
-
-SQLModel.metadata.create_all(engine)
--- data/csv_to_sql.py
+++ data/csv_to_sql.py
@@ -1,11 +1,9 @@
 from sqlmodel import SQLModel, Field, create_engine, Session, select
-from fastapi import FastAPI,UploadFile
+from fastapi import FastAPI, UploadFile
 import csv
 import uvicorn
 import io
 from app.services.soldier_services import add_soldier
-from sqlalchemy import Text, Date, Column, delete
-import  _sqlite3
 
 app = FastAPI()
 items = []
@@ -13,31 +11,26 @@ items = []
 engine = create_engine("sqlite:///database.db")
 
 
-
 @app.post("/assignWithCsv")
 def upload_csv(file: UploadFile):
-
     # checking that the file is csv file
     if file.content_type != "text/csv":
-         return {"error": "file must be a CSV"}
+        return {"invalid file": "file must be a csv"}
 
     content = file.file.read().decode("utf-8")
 
     reader = csv.reader(io.StringIO(content))
     next(reader)
     rows = list(reader)
-
-    for line in rows: # create and add soldier object from csv line
-         add_soldier(line)
-    return None
-
-
-
-
+    for line in rows:  # create and add soldier object from csv line
+        add_soldier(line)
+    return rows
 
 SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":  # mandatory
+
+def get_engine():
+    return engine # this is for the dal functions so they can access the db
+
+if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8010)
-
-
